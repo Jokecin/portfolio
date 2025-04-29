64b8f2f113c5b530e787a5b1cc922186d302e34c
Joke <matias.arayaj1@gmail.com>
2025-04-28 23:48:29 -0400

    Portafolio inicial

--- next.config.ts
+++ next.config.ts
@@ -3,5 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 };
-
+/** @type {import('next').NextConfig} */
+module.exports = {
+  output: 'export',        
+  images: { unoptimized: true },
+};
 export default nextConfig;
